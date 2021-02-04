fa15bf9f0fcc4670916058ebe2acb76533f609fe
468181 <468181@SAIT225790IT.ACDM.DS.SAIT.CA>
2021-02-04 13:57:00 -0700

    Finished Project Added edit parameter to switch between jsp's. Saved text to an external file, and read in file when the user views the page.

--- src/java/models/Note.java
+++ src/java/models/Note.java
@@ -10,5 +10,32 @@ package models;
  * @author 468181
  */
 public class Note {
-    
+
+    String title;
+    String contents;
+
+    public Note() {
+    }
+
+    public Note(String title, String contents) {
+        this.title = title;
+        this.contents = contents;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+
 }
